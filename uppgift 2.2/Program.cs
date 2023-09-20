ece44a3d288a5eb49ced35f61652474fc66f66b7
dean06001 <dean06001@E-PW0234KC>
2023-09-20 10:22:08 +0200

    Add project files.

---
+++ uppgift 2.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace uppgift_2_2
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin i meter?");
+            double Elin = double.Parse(Console.ReadLine());
+            Console.WriteLine("hur långt hoppade Alma i meter?");
+            string höjdText = Console.ReadLine();
+            double Alma = double.Parse(höjdText);
+            double skillnad = 1.45 - Elin;
+            Console.WriteLine("Elin hoppade" + skillnad + "meter längre");
+            Console.ReadLine();
+        }
+    }
+}
